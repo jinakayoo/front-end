b495ba562dd08d5c9375705b80f8997f6269d079
박유정 <p_1321@duksung.ac.kr>
2023-10-18 16:04:19 +0900

    나브바, 푸터바 로고 및 이메일 삽입

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import styled from "styled-components";
+import StarHubIconFooterbar from "../assets/icons/StarHubIconFooterbar.png";
 
 const FooterContainer = styled.div`
   width: 100%;
   background-color: #313866;
   display: flex;
   flex-direction: row;
-  justify-content: space-between;
+  justify-content: center;
+  align-items: center;
 `;
 
 const Section = styled.div`
-  // background-color: lightgray;
   text-align: center;
-  padding: 50px;
+  padding: 20px 50px 20px 50px;
+  margin: 0px 50px 0px 50px;
 `;
 
 const TitleContent = styled.p`
@@ -34,22 +36,22 @@ const Footer = () => {
   return (
     <FooterContainer>
       <Section>
-        <TitleContent>로고</TitleContent>
+          <img src={StarHubIconFooterbar} alt={'Logo'} style={{ width: 'auto', height: '200px'}} />
       </Section>
-      <Section>
-        <TitleContent>Delvelopers</TitleContent>
-        <TextContent>박유나</TextContent>
-        <TextContent>김가빈</TextContent>
-        <TextContent>류지예</TextContent>
-        <TextContent>박유정</TextContent>
-      </Section>
-      <Section>
-        <TitleContent>Contact</TitleContent>        
-        <TextContent>p_1321@duksung.ac.kr</TextContent>
-        <TextContent>p_1321@duksung.ac.kr</TextContent>
-        <TextContent>p_1321@duksung.ac.kr</TextContent>
-        <TextContent>p_1321@duksung.ac.kr</TextContent>
-      </Section>
+        <Section>
+          <TitleContent>Delvelopers</TitleContent>
+          <TextContent>박유나</TextContent>
+          <TextContent>김가빈</TextContent>
+          <TextContent>류지예</TextContent>
+          <TextContent>박유정</TextContent>
+        </Section>
+        <Section>
+          <TitleContent>Contact</TitleContent>        
+          <TextContent>yndbsk9372@gmail.com</TextContent>
+          <TextContent>kabin2205@gmail.com</TextContent>
+          <TextContent>devdaradara@gmail.com</TextContent>
+          <TextContent>p_1321@duksung.ac.kr</TextContent>
+        </Section>
     </FooterContainer>
   );
 };
