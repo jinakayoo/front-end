a107ee0dc49bd35190bf9f5ddace6da824aea510
daradara <devdaradara@gmail.com>
2023-11-05 00:36:02 +0900

    ApplicantListPage 화면 구성

---
+++ src/components/ProfileCard.js
@@ -0,0 +1,113 @@
+import React from 'react'
+import styled from "styled-components";
+import HumanIcon from "../assets/icons/HumanIcon.png";
+import PhoneIcon from "../assets/icons/PhoneIcon.png";
+import MessageIcon from "../assets/icons/MessageIcon.png";
+
+const PageContainer = styled.div`
+  height: 450px;
+  width: 300px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #ffffff;
+  border-radius: 20px;
+  box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.1);
+`
+
+const ProfileImageContainer = styled.div`
+  width: 200px;
+  height: 200px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #B3B4DC;
+  border-radius: 50%;
+  margin-bottom: 20px;
+`
+
+const ProfileImage = styled.img`
+  width: 150px;
+  height: 150px;
+  margin-top: 20px;
+  margin-bottom: 20px;
+`
+
+const RowDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  margin-top: 10px;
+`
+
+const NameText = styled.div`
+  font-size: 20px;
+  font-family: "SCDream6";
+  color: #313866;
+  margin-right: 5px;
+`
+
+const BirthText = styled.div`
+  font-size: 16px;
+  font-family: "SCDream4";
+  color: #7C8BBE;
+  margin-left: 5px;
+`
+
+const IntroText = styled.div`
+  font-size: 16px;
+  font-family: "SCDream4";
+  color: #313866;
+  margin-top: 10px;
+  margin-bottom: 10px;
+`
+
+const RowLeftDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  // to all the item start from left
+  justify-content: flex-start;
+  margin-top: 10px;
+`
+
+const PhoneText = styled.div`
+  font-size: 16px;
+  font-family: "SCDream4";
+  color: #313866;
+  /* margin-right: 60px; */
+`
+
+const EmailText = styled.div`
+  font-size: 16px;
+  font-family: "SCDream4";
+  color: #313866;
+`
+
+function ProfileCard({ name, age, email, phone_num, introduction, image }) {
+  return (
+    <PageContainer>
+      <ProfileImageContainer>
+        <ProfileImage src={HumanIcon} />
+      </ProfileImageContainer>
+      <RowDiv>
+        <NameText>{name}</NameText>
+        <BirthText>{age}세</BirthText>
+      </RowDiv>
+      <IntroText>{introduction}</IntroText>
+      <RowLeftDiv>
+        <img src={PhoneIcon} style={{ width: "15px", height: "15px", marginRight: "10px" }} />
+        <PhoneText>{phone_num}</PhoneText>
+      </RowLeftDiv>
+      <RowLeftDiv>
+        <img src={MessageIcon} style={{ width: "17px", height: "17px", marginRight: "10px" }} />
+        <EmailText>{email}</EmailText>
+      </RowLeftDiv>
+    </PageContainer>
+  );
+}
+
+
+export default ProfileCard
--- src/pages/ApplicantListPage.js
+++ src/pages/ApplicantListPage.js
@@ -1,15 +1,60 @@
 import React from 'react';
 import styled from "styled-components";
+import ProfileCard from '../components/ProfileCard';
+import PickIcon from "../assets/icons/PickIcon.png";
+import { applicantData } from "../assets/data/applicantdata";
 
 const PageContainer = styled.div`
-  height: 300px;
-`
+  height: 1100px;
+  background-color: #f6f1fb;
+`;
+
+const PickContainer = styled.div`
+  flex-direction: row;
+  display: flex;
+  align-items: center;
+`;
+
+const PickText = styled.div`
+  font-size: 50px;
+  font-family: "GmarketSans";
+  color: #7c8bbe;
+  margin-left: 10px;
+`;
+
+const ListContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr); 
+  gap: 20px;
+  padding: 20px;
+`;
+
+const PickTitle = () => {
+  return (
+    <PickContainer>
+      <img src={PickIcon} style={{width: "45px", height: "45px"}} />
+      <PickText>Pick!</PickText>
+    </PickContainer>
+  );
+};
 
 const ApplicantListPage = () => {
-
   return (
     <PageContainer>
-      지원자리스트페이지
+      <PickTitle />
+      <ListContainer>
+      {applicantData.map((applicant, index) => (
+        <ProfileCard
+          key={index}
+          name={applicant.name}
+          age={applicant.age}
+          email={applicant.email}
+          phone_num={applicant.phone_num}
+          introduction={applicant.introduction}
+          image={applicant.image}
+        />
+      ))} 
+      </ListContainer>
     </PageContainer>
   );
 };
