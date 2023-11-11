27c3e463655ba6793f21280f8d18ec0f6b73bebb
daradara <devdaradara@gmail.com>
2023-11-12 02:43:49 +0900

    주소 입력 팝업

--- src/pages/StudyRecruitPage.js
+++ src/pages/StudyRecruitPage.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from "styled-components";
-
 import StarIcon from "../assets/icons/StarIcon.png";
+import DaumPostcode from 'react-daum-postcode';
 
 const PageContainer = styled.div`
   padding: 50px 100px 50px 100px;
@@ -29,7 +29,7 @@ const HorizontalLine = styled.div`
   margin: 0px;
 `;
 
-  const TitleText = styled.p`
+const TitleText = styled.p`
   margin: 0px 0px 0px 10px;
   font-size: 24px;
   font-family: 'GmarketSans';
@@ -103,12 +103,85 @@ const RowWrapper = styled.div`
   align-items: center;
 `;
 
+const AddressInput = styled.input`
+  margin-top: 5px;
+  margin-bottom: 15px;
+  padding: 15px;
+  border: 3px solid #B3B4DC;
+  border-radius: 10px;
+  width: 28vw;
+  font-size: 18px;
+  font-family: 'SCDream4', sans-serif;
+`;
+
+const FindAddressButton = styled.button`
+  margin-left: 20px;
+  width: 100px;
+  height: 50px;
+  border: none;
+  border-radius: 10px;
+  background-color: #B6B6B6;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+  font-family: 'SCDream6';
+  color: white;
+  font-size: 16px;
+  cursor: pointer;
+`;
+
+function FindAddress({ setAddressObj }) {
+  const handleComplete = (data) => {
+    let fullAddress = data.address;
+    let extraAddress = '';
+    let localAddress = data.sido + ' ' + data.sigungu;
+
+    if (data.addressType === 'R') {
+      if (data.bname !== '') {
+        extraAddress += data.bname;
+      }
+      if (data.buildingName !== '') {
+        extraAddress += (extraAddress !== '' ? `, ${data.buildingName}` : data.buildingName);
+      }
+
+      fullAddress += (extraAddress !== '' ? `(${extraAddress})` : '');
+
+      const regex = /\(가\)/;
+      fullAddress = fullAddress.replace(regex, '');
+
+      setAddressObj({
+        areaAddress: '',
+        townAddress: fullAddress,
+      });
+
+      const addressInput = document.getElementById('addressInput');
+      if (addressInput) {
+        addressInput.value = fullAddress;
+      }
+    }
+  };
+
+  const handleClick = () => {
+    new window.daum.Postcode({
+      oncomplete: handleComplete,
+    }).open();
+  };
+
+  return (
+    <FindAddressButton type="button" onClick={handleClick}>
+      주소 찾기
+    </FindAddressButton>
+  );
+}
+
 const StudyRecruitPage = () => {
+  const [addressObj, setAddressObj] = useState({
+    areaAddress: '',
+    townAddress: '',
+  });
 
   return (
     <PageContainer>
       <RowWrapper>
-        <img src={StarIcon} alt={'Star Icon'} style={{ width: 'auto', height: '20px'}} />
+        <img src={StarIcon} alt={'Star Icon'} style={{ width: 'auto', height: '20px' }} />
         <TitleText>프로젝트 기본 정보</TitleText>
       </RowWrapper>
       <HorizontalLine />
@@ -116,44 +189,45 @@ const StudyRecruitPage = () => {
         <Inputbox>
           <InputWrapper>
             <TextInput>모집 구분</TextInput>
-            <Input type="text" placeholder="스터디와 프로젝트 중 선택해주세요."/>
+            <Input type="text" placeholder="스터디와 프로젝트 중 선택해주세요." />
           </InputWrapper>
           <InputWrapper>
             <TextInput>기술 스택</TextInput>
-            <Input type="text" placeholder="사용되는 기술 스택을 입력해주세요. ex) 리액트, 스프링..."/>
+            <Input type="text" placeholder="사용되는 기술 스택을 입력해주세요. ex) 리액트, 스프링..." />
           </InputWrapper>
         </Inputbox>
         <Inputbox>
           <InputWrapper>
             <TextInput>모집 인원</TextInput>
-            <Input type="text"placeholder="모집 인원 수를 입력해주세요. ex) 3~5"/>
+            <Input type="text" placeholder="모집 인원 수를 입력해주세요. ex) 3~5" />
           </InputWrapper>
           <InputWrapper>
             <TextInput>진행 기간</TextInput>
-            <Input type="text"placeholder="진행 기간을 입력해주세요."/>
+            <Input type="text" placeholder="진행 기간을 입력해주세요." />
           </InputWrapper>
         </Inputbox>
         <Inputbox>
           <InputWrapper>
             <TextInput>진행 장소</TextInput>
-            <Input type="text"placeholder="주소를 입력해주세요."/>
+            <AddressInput type="text" id="addressInput" placeholder="주소를 입력해주세요." />
+            <FindAddress setAddressObj={setAddressObj} />
           </InputWrapper>
           <InputWrapper>
             <TextInput>모집 마감일</TextInput>
-            <Input type="text"placeholder="**** - ** - **"/>
+            <Input type="text" placeholder="**** - ** - **" />
           </InputWrapper>
         </Inputbox>
       </Info>
       <RowWrapper>
-        <img src={StarIcon} alt={'Star Icon'} style={{ width: 'auto', height: '20px'}} />
+        <img src={StarIcon} alt={'Star Icon'} style={{ width: 'auto', height: '20px' }} />
         <TitleText>프로젝트 소개</TitleText>
       </RowWrapper>
       <HorizontalLine />
       <Intro>
         <TextInput>제목</TextInput>
-        <Input type="text"placeholder="제목을 입력해주세요."/>
+        <Input type="text" placeholder="제목을 입력해주세요." />
         <TextInput>내용</TextInput>
-        <Textarea type="text" placeholder="내용을 입력해주세요."/>
+        <Textarea type="text" placeholder="내용을 입력해주세요." />
       </Intro>
       <ButtonContainer>
         <Button>글 등록</Button>
