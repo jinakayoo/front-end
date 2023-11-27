f75fdc146ac9221e68b8952261934df54494aaec
daradara <devdaradara@gmail.com>
2023-11-28 03:32:59 +0900

    더미 데이터 변경

--- src/assets/data/mapdata.js
+++ src/assets/data/mapdata.js
@@ -1,7 +1,7 @@
 export const mapdata = [
   {
     "skill": "리액트",
-    "place": "스머프동산",
+    "place": "서울 도봉구 삼양로144길 33(쌍문동 419, 스머프동산)",
     "latitude": 37.651752,
     "longitude": 127.015522,
     "progress": "1",
@@ -11,11 +11,11 @@ export const mapdata = [
     "done": false,
     "title": "리액트 스터디 모집",
     "content": "같이해요",
-    "userId": 1
+    "userId": 5
   },
   {
     "skill": "스프링",
-    "place": "컴포즈",
+    "place": "서울 도봉구 삼양로 538 1층(쌍문동, 컴포즈커피)",
     "latitude": 37.650885,
     "longitude": 127.013202,
     "progress": "1",
@@ -25,11 +25,11 @@ export const mapdata = [
     "done": false,
     "title": "스프링 스터디 모집",
     "content": "같이해요",
-    "userId": 1
+    "userId": 3
   },
   {
     "skill": "리액트",
-    "place": "블랙다운",
+    "place": "서울 강북구 삼양로 528-1 1층(우이동, 블랙다운)",
     "latitude": 37.65013,
     "longitude": 127.013586,
     "progress": "1",
@@ -37,23 +37,9 @@ export const mapdata = [
     "deadline": "2023-06-17",
     "type": "프로젝트",
     "done": false,
-    "title": "리액트 플젝 기획",
-    "content": "같이해요",
-    "userId": 1
-  },
-  {
-    "skill": "리액트",
-    "place": "입구",
-    "latitude": 37.651686,
-    "longitude": 127.014907,
-    "progress": "1",
-    "peopleNum": "3",
-    "deadline": "2023-06-17",
-    "type": "프로젝트",
-    "done": false,
     "title": "StarHub 프론트엔드",
     "content": "같이해요",
-    "userId": 1
+    "userId": 4
   },
   {
     "skill": "스프링",
@@ -67,6 +53,6 @@ export const mapdata = [
     "done": false,
     "title": "StarHub 백엔드",
     "content": "같이해요",
-    "userId": 1
+    "userId": 6
   }
 ];
---
+++ src/assets/data/userData.js
@@ -0,0 +1,56 @@
+export const userData = [
+  {
+    "loginId": "1234",
+    "password": "1234",
+    "name": "개설자",
+    "age": 23,
+    "email": "devdaradara@gmail.com",
+    "phoneNum": "01012345678",
+    "introduction": "개설자입니다:)"
+  },
+  {
+    "loginId": "12345",
+    "password": "12345",
+    "name": "지원자",
+    "age": 23,
+    "email": "devdaradara@gmail.com",
+    "phoneNum": "01012345678",
+    "introduction": "지원자입니다:)"
+  },
+  {
+    "loginId": "2345",
+    "password": "2345",
+    "name": "김가빈",
+    "age": 25,
+    "email": "gabin@example.com",
+    "phoneNum": "01098765432",
+    "introduction": "김가빈입니다:)"
+  },
+  {
+    "loginId": "3456",
+    "password": "3456",
+    "name": "박유정",
+    "age": 30,
+    "email": "youjeong@example.com",
+    "phoneNum": "01056781234",
+    "introduction": "박유정입니다:)"
+  },
+  {
+    "loginId": "4567",
+    "password": "4567",
+    "name": "박유나",
+    "age": 28,
+    "email": "yuna@example.com",
+    "phoneNum": "01024681357",
+    "introduction": "박유나입니다:)"
+  },
+  {
+    "loginId": "5678",
+    "password": "5678",
+    "name": "류지예",
+    "age": 26,
+    "email": "jiye@example.com",
+    "phoneNum": "01012347856",
+    "introduction": "류지예입니다:)"
+  },
+];
